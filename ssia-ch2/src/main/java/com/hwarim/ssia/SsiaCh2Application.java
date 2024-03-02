c08f9e28dd170fde1101b671f3b2b4040a3e5933
hwarim-huray <hr.ryu@huray.net>
2024-03-02 16:51:54 +0900

    [chap2]2.기본 구성

---
+++ ssia-ch2/src/main/java/com/hwarim/ssia/SsiaCh2Application.java
@@ -0,0 +1,13 @@
+package com.hwarim.ssia;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsiaCh2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsiaCh2Application.class, args);
+	}
+
+}
